35eff7c3a700e3f6bc7ed916293ebb4a4aaeff5e
YEONXX <yskim4085@naver.com>
2023-07-04 15:00:16 +0900

    Revert "MFC Sample code 업로드"
    This reverts commit e3908027164658c900d0b22e38c9306ef322f9f1.

--- MFC_Dev/Keyboard/EnglishProcess.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class CEnglishProcess
-{
-public:
-	bool		IsOneKeyToMultiAlphabetLower(const CString str1, const CString str2, CString* str);
-	bool		IsOneKeyToMultiAlphabetUpper(const CString str1, const CString str2, CString* str);
-
-};
-
